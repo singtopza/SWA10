b31b3d9184ccea73399769aba59597afa8315da3
singtopza <59428798+singtopza@users.noreply.github.com>
2021-10-22 22:40:46 +0700

    Update SingletionCilent.java

--- src/SingletionCilent.java
+++ src/SingletionCilent.java
@@ -1,4 +1,6 @@
-
 public class SingletionCilent {
-
+	public static void main(String [] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
 }
