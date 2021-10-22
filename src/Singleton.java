6563b57ec44db4763728293ac00cb9321d1d506e
singtopza <59428798+singtopza@users.noreply.github.com>
2021-10-22 22:40:36 +0700

    Update Singleton.java

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,4 +1,18 @@
+    public class Singleton {
+	
+    private static Singleton uniqueInstance;
 
-public class Singleton {
+    private Singleton() {}
 
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+  
+
+    public String getDescription () {
+    return "I'm a classic Singleton!";
+    }
 }
